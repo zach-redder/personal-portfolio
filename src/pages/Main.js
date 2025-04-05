5a0479539a0c31cdd580718d7003d086946bf609
zmr7 <zmr7@calvin.edu>
2025-04-05 15:30:42 -0400

    New newsletter feature and redirection

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 
 import { AOSElement } from "../functions/normalFunctions";
+import { Helmet } from "react-helmet-async";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
@@ -55,6 +56,10 @@ function MainPage() {
     }
 
     return (
+        <>
+        <Helmet>
+            <title>Zach Redder</title>
+        </Helmet>
         <div className="main-container">
             <Navbar></Navbar>
             <div className="social-icons">
@@ -268,6 +273,7 @@ function MainPage() {
             </section>
             <Footer></Footer>
         </div>
+    </>
     );
 }
 
