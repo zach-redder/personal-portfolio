6986f2bc5fcc13b471a39e994eb2d72f76629fbe
zmr7 <zmr7@calvin.edu>
2025-04-10 19:36:11 -0400

    BUG FIXES

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,35 @@
-import '../stylesheets/About.css';  // Import global styles here
-import '../stylesheets/Company.css'; // Import global styles here
-import '../stylesheets/ContactBox.css'; // Import global styles here
-import '../stylesheets/Main.css'; // Import global styles here
-import '../stylesheets/Navbar.css'; // Import global styles here
-import '../stylesheets/Newsletter.css'; // Import global styles here
-import '../stylesheets/Projects.css'; // Import global styles here
-import '../stylesheets/Skills.css'; // Import global styles here
-import '../stylesheets/Socials.css'; // Import global styles here
-import '../stylesheets/StartScreen.css'; // Import global styles here
+import '../stylesheets/About.css';
+import '../stylesheets/Company.css';
+import '../stylesheets/ContactBox.css';
+import '../stylesheets/Main.css';
+import '../stylesheets/Navbar.css';
+import '../stylesheets/Newsletter.css';
+import '../stylesheets/Projects.css';
+import '../stylesheets/Skills.css';
+import '../stylesheets/Socials.css';
+import '../stylesheets/StartScreen.css';
 
-import React from 'react';
-import Head from 'next/head'; // Import the Head component from next/head
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({ once: true });
+    AOS.refresh();
+  }, []);
+
   return (
     <>
       <Head>
         <title>Zach Redder</title>
         <meta name="description" content="Web site created using create-react-app" />
         <meta name="theme-color" content="#000000" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/zr3.png" />
         <link rel="apple-touch-icon" href="/zr3.png" />
         <link rel="manifest" href="/manifest.json" />
